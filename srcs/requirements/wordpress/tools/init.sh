43a0aa969d1a0481c2edf48292aee6f33717b73d
Narancia34 <tahagamraoui@gmail.com>
2026-08-16 20:22:20 +0100

    Revert "fix(wordpress): init script wrong env variable name"
    This reverts commit 22e85b9d89a3fb606f5bb17f18ec483d6c102b9b.

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -37,7 +37,7 @@ if [ ! -f wp-config.php ]; then
     wp core install --allow-root \
         --url="https://${DOMAIN_NAME}" \
         --title="Inception 42" \
-        --admin_user="${WP_ADMIN_USER}" \
+        --admin_user="${WP_ADMIN}" \
         --admin_password="${WP_ADMIN_PASSWORD}" \
         --admin_email="${WP_ADMIN_EMAIL}"
 
